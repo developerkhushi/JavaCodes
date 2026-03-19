13997a2305cb4e866aafc4f6bfb4a71c34f88741
Khushboo Singh <khushboosingh@Khushboos-MacBook-Air.local>
2026-03-19 23:47:00 +0530

    First Occurence of sub string implemented

--- src/com/dsa/string/FirstOccurenceString.java
+++ src/com/dsa/string/FirstOccurenceString.java
@@ -1,4 +1,26 @@
 package com.dsa.string;
 
 public class FirstOccurenceString {
+    public static void main(String[] args) {
+        String s = "hamsemster";
+        String sub = "mst";
+        int ans = -1;
+
+        for (int i = 0; i <= s.length() - sub.length(); i++) {
+            char firstCharOfSub = sub.charAt(0);
+            if (firstCharOfSub == s.charAt(i)) {
+                int k;
+                for (k = 1; k < sub.length(); k++) {
+                    if (sub.charAt(k) != s.charAt(i + k)) {
+                        break;
+                    }
+                }
+                if (k == sub.length()) {
+                    ans = i;
+                    break;
+                }
+            }
+        }
+        System.out.println(ans);
+    }
 }
